043cbb5413951f158088fcd1ecbec16109239930
syglee7 <syglee7@gmail.com>
2020-02-17 14:28:25 +0900

    2020.02.17 redux saga

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
 import withRedux from 'next-redux-wrapper';
-
-import AppLayout from '../components/AppLayout';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+
+import AppLayout from '../components/AppLayout';
 import reducer from '../reducers';
+import rootSaga from '../sagas';
 
 const NodeBird = ({ Component, store }) => {
     return (
@@ -28,11 +30,15 @@ NodeBird.propTypes = {
 };
 
 export default withRedux((initialState, options) => {
-    const middlewares = [];
-    const enhancer = compose(
+    const sagaMiddleware = createSagaMiddleware();
+    const middlewares = [sagaMiddleware];
+    const enhancer = process.env.NODE_ENV === 'production'
+    ? compose(applyMiddleware(...middlewares),)
+    : compose(
         applyMiddleware(...middlewares),
         !options.isServer && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
     );
     const store = createStore(reducer, initialState, enhancer);
+    sagaMiddleware.run(rootSaga);
     return store;
 })(NodeBird);
