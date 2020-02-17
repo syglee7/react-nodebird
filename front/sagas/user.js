554cc038595a623205f2783ac94b104d7dd7edd3
syglee7 <syglee7@gmail.com>
2020-02-17 18:27:31 +0900

    2020.02.17 use redux saga

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,4 +1,4 @@
-import { all, fork, takeLatest } from 'redux-saga/effects';
+import { all, fork, takeLatest, call, put } from 'redux-saga/effects';
 import { LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE } from "../reducers/user";
 
 function loginAPI() {
@@ -10,20 +10,27 @@ function* login() {
         yield call(loginAPI); // call 은 함수 동기적 호출
         yield put({ // put 은 액션 dispatch 동일
             type: LOG_IN_SUCCESS,
-        })
+        });
     } catch (e) { // loginAPI 실패
         console.error(e);
         yield put({
             type: LOG_IN_FAILURE,
-        })
+        });
     }
 }
-function* watchLogin() {
-    yield takeLatest(LOG_IN, login)
+
+function* watchSignUp() {
+
 }
 
-export default function* userSaga () {
+function* watchLogin() {
+    yield takeLatest(LOG_IN, login);
+}
+
+export default function* userSaga() {
     yield all([
-        fork(watchLogin), // fork 는 함수 비동기적 호출
+        //fork(watchLogin), // fork 는 함수 비동기적 호출
+        watchLogin(),
+        watchSignUp(),
     ]);
 }
